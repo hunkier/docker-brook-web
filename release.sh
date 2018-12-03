5de0a2922dd53015a4f4aa5297df23a144f735a2
huangkui <huangkuier@gmail.com>
2018-12-03 21:10:54 +0800

    add auto release

--- release.sh
+++ release.sh
@@ -5,8 +5,8 @@ docker pull alpine
 docker pull centos
 
 docker build -t hunkier/brook-web:ubuntu ./ubuntu/ && docker push hunkier/brook-web:ubuntu
-docker build -t hunkier/brook-web:alpine ./ubuntu/ && docker push hunkier/brook-web:alpine
-docker build -t hunkier/brook-web:centos ./ubuntu/ && docker push hunkier/brook-web:centos
+docker build -t hunkier/brook-web:alpine ./alpine/ && docker push hunkier/brook-web:alpine
+docker build -t hunkier/brook-web:centos ./centos/ && docker push hunkier/brook-web:centos
 
 docker tag hunkier/brook-web:alpine hunkier/brook-web:latest
 docker push hunkier/brook-web:latest
